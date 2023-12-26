6119ba17b942d8a58cb7991a24ff4f75a4fe5071
Andy Lai <laihochi@gmail.com>
2023-12-26 20:31:59 +0000

    Update questions for inquirer

--- index.js
+++ index.js
@@ -13,3 +13,103 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const managerQuestions = [
+{
+    type: 'input', 
+    name: 'name',
+    message: "Name: ",
+},    
+{
+    type: 'input', 
+    name: 'id',
+    message: "Employee ID: ",
+},   
+{
+    type: 'input', 
+    name: 'email',
+    message: "Email address: ",
+},   
+{
+    type: 'input',
+    name: 'officenumber',
+    message: "Office number: "
+}
+];
+
+const enginnerQuesteions = [
+    {
+        type: 'input', 
+        name: "name",
+        message: "Engineer's Name: ",
+    },    
+    {
+        type: 'input', 
+        name: 'id',
+        message: "ID: ",
+    },   
+    {
+        type: 'input', 
+        name: 'email',
+        message: "Email: ",
+    },   
+    {
+        type: 'input',
+        name: 'github',
+        message: "GitHub username: "
+    }
+];
+
+const internQuestions = [
+    {
+        type: 'input', 
+        name: 'name',
+        message: "Intern's name: ",
+    },
+    {
+        type: 'input', 
+        name: 'id',
+        message: "ID: ",
+    },    
+    {
+        type: 'input', 
+        name: 'email',
+        message: "Email: ",
+    },        
+    {
+        type: 'input',
+        name: 'school',
+        message: "School: "
+    }
+];
+
+const menu = [
+{
+    type: 'list',
+    name: 'option',
+    message: "Options: ",
+    choices: [
+        {
+            key: 'e',
+            name: 'Add an engineer',
+            value: 'engineer'
+        },
+        {
+            key: 'i',
+            name: 'Add an intern',
+            value: 'intern'
+        },
+        {
+            key: 'q',
+            name: 'Finish building the team',
+            value: 'quit'
+        }
+    ],
+    default: ''
+}
+];
+
+function init(){
+    
+};
+
+init();
